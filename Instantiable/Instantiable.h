bd0ea790521ebb31d66abda8beb0d43e5b9043f7
Marcin Węglarz <marcin.weglarz01@gmail.com>
2017-10-16 11:12:50 +0200

    init

---
+++ Instantiable/Instantiable.h
@@ -0,0 +1,19 @@
+//
+//  Instantiable.h
+//  Instantiable
+//
+//  Created by pc_mweglarz on 16/10/2017.
+//  Copyright © 2017 mweglarz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Instantiable.
+FOUNDATION_EXPORT double InstantiableVersionNumber;
+
+//! Project version string for Instantiable.
+FOUNDATION_EXPORT const unsigned char InstantiableVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Instantiable/PublicHeader.h>
+
+
