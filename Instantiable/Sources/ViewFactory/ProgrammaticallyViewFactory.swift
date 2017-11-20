34f7045fc4e9391298abfc428bd0384a72e93a5d
Marcin Węglarz <marcin.weglarz01@gmail.com>
2017-11-20 11:58:17 +0100

    Add ProgrammaticallyViewFactory

---
+++ Instantiable/Sources/ViewFactory/ProgrammaticallyViewFactory.swift
@@ -0,0 +1,17 @@
+//
+//  ProgrammaticallyViewFactory.swift
+//  Instantiable
+//
+//  Created by pc_mweglarz on 20/11/2017.
+//  Copyright © 2017 mweglarz. All rights reserved.
+//
+
+import Foundation
+
+open class ProgrammaticallyViewFactory: ViewFactory {
+    
+    override open func create<Controller>() -> Controller where Controller: UIViewController {
+        let viewController = Controller()
+        return viewController
+    }
+}
